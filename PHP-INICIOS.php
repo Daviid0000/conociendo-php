4f9ed35d795cda7ed1eba7bacb40145fd10e6bf0
David Liva <livadavid78@gmail.com>
2024-08-20 14:58:02 -0300

    Se ha inicializado un mini proyecto para conocer php

---
+++ PHP-INICIOS.php
@@ -0,0 +1,34 @@
+<?php
+    # maneras de imprimir texto en pantalla
+    // echo("hola mundo");
+    // print_r("cargando...............................................................");
+    // var_dump("dasdas");
+    
+    $contador = rand(1, 3);
+
+    // for ($contador=1; $contador < 2; $contador++) { 
+    //     echo("El número es:" . $contador);
+    //     # code...
+    // }
+
+    for ($i=0; $i < $contador ; $i++) { 
+        # code...
+        echo("El número es:" . $contador);
+    }
+
+    // switch ($variable) {
+    //     case 'value':
+    //         # code...
+    //         break;
+        
+    //     default:
+    //         # code...
+    //         break;
+    // }
+    // print_r($contador)
+?>
+
+<!--
+    Generación de datos de prueba
+
+-->
